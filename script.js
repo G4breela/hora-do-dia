082d99329a790cedd23c163f3c902bba45e37bc9
G4breela <gs.silva01@outlook.com>
2022-05-11 16:15:06 -0300

    alteração no script

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ function carregar() {
     var minuto = data.getMinutes()
     var segundo = data.getSeconds()
     var intervald = window.setInterval(() => {carregar()},1000);
-    msg.innerHTML = `Agora são ${hora}:${minuto}:${segundo}`
+    msg.innerHTML = `Horário: ${hora}:${minuto}:${segundo}`
     if (hora >= 0 && hora < 12){
         foto.src = '/img/manhã.png'
         document.body.style.background = '#9fb489'
-    }else if (hora > 12 && hora < 18){
+        msg.innerHTML = `Horário: ${hora}:0${minuto}`
+    }else if (hora > 12 && hora < 18) {
         foto.src = '/img/tarde.png'
         document.body.style.background = '#c96412'
+        
     } else{
        foto.src = '/img/noite.png'
         document.body.style.background = '#141823'
     }
+    if (minuto < 10) {
+        msg.innerHTML = `Horário: ${hora}:0${minuto}:0${segundo}`
+    }
 }
+
