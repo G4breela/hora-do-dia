f715939dd85dddf185db1cadcfe9cdb9fe78e732
G4breela <gs.silva01@outlook.com>
2022-05-11 15:52:11 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var foto = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    var segundo = data.getSeconds()
+    var intervald = window.setInterval(() => {carregar()},1000);
+    msg.innerHTML = `Agora são ${hora}:${minuto}:${segundo}`
+    if (hora >= 0 && hora < 12){
+        foto.src = 'manhã.png'
+        document.body.style.background = '#9fb489'
+    }else if (hora > 12 && hora < 18){
+        foto.src = 'tarde.png'
+        document.body.style.background = '#c96412'
+    } else{
+       foto.src = 'noite.png'
+        document.body.style.background = '#141823'
+    }
+}
