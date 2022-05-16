323a497409b59e507a7bea48605251513dacace8
G4breela <gs.silva01@outlook.com>
2022-05-16 12:52:04 -0300

    concerta erro na imagem

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ function carregar() {
     var intervald = window.setInterval(() => {carregar()},1000);
     msg.innerHTML = `Horário: ${hora}:${minuto}:${segundo}`
     if (hora >= 0 && hora < 12){
-        foto.src = '../img/manhã.png'
+        foto.src = 'img/manhã.png'
         document.body.style.background = '#9fb489'
         
-    }else if (hora > 12 && hora < 18) {
-        foto.src = '../img/tarde.png'
+    }else if (hora >= 12 && hora <= 18) {
+        foto.src = 'img/tarde.png'
         document.body.style.background = '#c96412'
-        
+     
     } else{
-       foto.src = '../img/noite.png'
+       foto.src = 'img/noite.png'
         document.body.style.background = '#141823'
     }
     if (minuto < 10){
